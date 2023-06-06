e0cefa8fdd881dea47b2e36c8b7a304c2294b41b
Emanuel Rodríguez <75333105+Emanuel-TC@users.noreply.github.com>
2023-06-06 10:16:50 -0600

    Update main.js

--- my_server/js/main.js
+++ my_server/js/main.js
@@ -224,7 +224,7 @@ const GraficaLineaFrecuencia = ({ data }) => {
                             label: `Línea de Frecuencia: ${ultimoDato.y}`,
                             data: data.map((item) => item.y),
                             borderColor: "rgba(75, 192, 192, 1)",
-                            fill: false,
+                            fill: true,
                         },
                     ],
                 },
@@ -249,9 +249,6 @@ const GraficaLineaFrecuencia = ({ data }) => {
         data.length > 0
     );
 };
-
-
-
 const GraficaPotenciaActivaAparenteReactiva = ({ data }) => {
     let chart = null;
 
@@ -353,8 +350,85 @@ const GraficaPotenciaActivaAparenteReactiva = ({ data }) => {
         data.length > 0
     );
 };
-//Factor de potencia
+//Factor de potencia 8 datos
 const GraficaFactorPotencia = ({ data }) => {
+    let chart = null;
+
+    const updateChart = () => {
+        fetch('/api/data/get')
+            .then(r => r.json())
+            .then(r => {
+                // Extraer los datos necesarios del resultado
+                const newData = r.map((item) => ({
+                    x: `${item.Fecha} ${item.Hora}`,
+                    y: item["Factor de Potencia"]
+                }));
+
+                // Tomar solo los últimos 15 datos
+                const latestData = newData.slice(Math.max(newData.length - 8, 0));
+
+                // Extraer los datos actuales de la gráfica
+                const currentLabels = chart.data.labels;
+                const currentData = chart.data.datasets[0].data;
+
+                // Asegurarse de que los datos sean diferentes de los datos actuales de la gráfica
+                if (JSON.stringify(latestData.map(item => item.x)) !== JSON.stringify(currentLabels) ||
+                    JSON.stringify(latestData.map(item => item.y)) !== JSON.stringify(currentData)) {
+                    // Actualizar los datos
+                    chart.data.labels = latestData.map((item) => item.x);
+                    chart.data.datasets[0].data = latestData.map((item) => item.y);
+
+                    // Actualizar el label con el último dato
+                    const ultimoDato = latestData[latestData.length - 1];
+                    chart.data.datasets[0].label = `Factor de Potencia: ${ultimoDato.y}`;
+
+                    chart.update();
+                }
+            });
+    };
+
+    useEffect(() => {
+        if (data.length > 0) {
+            const ctx = document.getElementById("graficaFactorPotencia").getContext("2d");
+            const ultimoDato = data[data.length - 1];
+            chart = new Chart(ctx, {
+                type: "line",
+                data: {
+                    labels: data.map((item) => item.x),
+                    datasets: [
+                        {
+                            label: `Factor de Potencia: ${ultimoDato.y}`,
+                            data: data.map((item) => item.y),
+                            borderColor: "rgba(75, 192, 192, 1)",
+                            fill: true,
+                        },
+                    ],
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                        },
+                    },
+                },
+            });
+
+            // Llamar a updateChart cada 500 ms
+            const intervalId = setInterval(updateChart, 500);
+
+            // Limpiar el intervalo cuando el componente se desmonte
+            return () => clearInterval(intervalId);
+        }
+    }, [data]);
+
+    return (
+        data.length > 0
+    );
+};
+//fin FP8D
+
+//Factor de potencia último
+const GraficaFactorPotenciaUltimo = ({ data }) => {
     let chart = null;
 
     const updateChart = () => {
@@ -385,7 +459,7 @@ const GraficaFactorPotencia = ({ data }) => {
             const ultimoFactorPotencia = data[data.length - 1]["Factor de Potencia"];
             const restante = 1 - ultimoFactorPotencia;
 
-            const ctx = document.getElementById("graficaFactorPotencia").getContext("2d");
+            const ctx = document.getElementById("graficaFactorPotenciaUltimo").getContext("2d");
             chart = new Chart(ctx, {
                 type: "doughnut",
                 data: {
@@ -442,11 +516,168 @@ const DataList = ({ data }) => (
     </ul>
   `
 );
+//
 
+//
+// Realizar una solicitud a la API
+fetch("/api/data/get")
+    .then(response => response.json()) // Convertir la respuesta a formato JSON
+    .then(data => { // Trabajar con los datos obtenidos
+        let table = document.querySelector("table"); // Seleccionar la tabla
+        let tbody = document.createElement("tbody"); // Crear el cuerpo de la tabla
+
+        // Iterar sobre los datos
+        for (let item of data) {
+            // Crear una nueva fila
+            let row = document.createElement("tr");
+
+            // Agregar las columnas a la fila
+            row.innerHTML = `
+        <td>${item["ID"]}</td>
+        <td>${item["Fecha"]}</td>
+        <td>${item["Hora"]}</td>
+        <td>${item["Voltaje RMS"]}</td>
+        <td>${item["Línea de Frecuencia"]}</td>
+        <td>${item["Factor de Potencia"]}</td>
+        <td>${item["Corriente RMS"]}</td>
+        <td>${item["Potencia Activa"]}</td>
+        <td>${item["Potencia Reactiva"]}</td>
+        <td>${item["Potencia Aparente"]}</td>
+      `;
+
+            // Agregar la fila al cuerpo de la tabla
+            tbody.appendChild(row);
+        }
+
+        // Agregar el cuerpo de la tabla a la tabla
+        table.appendChild(tbody);
+    })
+    .catch(error => console.error('Error:', error));
+
+//
+function searchDate() {
+    var input_startDate, input_stopDate, table, tr, i;
+
+    // get the values and convert to date
+    input_startDate = new Date(document.getElementById("date-start").value);
+    input_stopDate = new Date(document.getElementById("date-stop").value);
+
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        // you need to get the text and convert to date
+        let td_date = new Date(tr[i].getElementsByTagName("td")[0].textContent);
+
+        // now you can compare dates correctly
+        if (td_date) {
+            if (td_date >= input_startDate && td_date <= input_stopDate) {
+                // show the row by setting the display property
+                tr[i].style.display = 'table-row;';
+            } else {
+                // hide the row by setting the display property
+                tr[i].style.display = 'none';
+            }
+        }
+
+    }
+}
+
+function searchDateAlternative() {
+    // get the values and convert to date
+    const input_startDate = new Date(document.getElementById("date-start").value);
+    const input_stopDate = new Date(document.getElementById("date-stop").value);
+
+    // only process table body rows, ignoring footer/headers
+    const tr = document.querySelectorAll("table tbody tr")
+
+    for (let i = 0; i < tr.length; i++) {
+        // ensure we have a relevant td
+        let td = tr[i].getElementsByTagName("td");
+        if (!td || !td[0]) continue;
+
+        // you need to get the text and convert to date
+        let td_date = new Date(td[0].textContent);
+
+        // now you can compare dates correctly
+        if (td_date) {
+            if (td_date >= input_startDate && td_date <= input_stopDate) {
+                // show the row by setting the display property
+                tr[i].style.display = 'table-row;';
+            } else {
+                // hide the row by setting the display property
+                tr[i].style.display = 'none';
+            }
+        }
+
+    }
+}
+
+// Obtén los elementos de la página
+let startDateInput = document.getElementById("start-date");
+let endDateInput = document.getElementById("end-date");
+let filterButton = document.getElementById("filter-button");
+let table = document.querySelector("table");
+
+// Define una función que se encargará de realizar la solicitud
+
+
+//
 
 const App = function (props) {
     const [config, setConfig] = useState({});
     const [data, setData] = useState([]);
+    /*const filterData = () => {
+        alert('Button was clicked');
+        // Obtén las fechas seleccionadas por el usuario
+        let startDate = startDateInput.value;
+        let endDate = endDateInput.value;
+
+        // Limpia la tabla antes de agregar las nuevas filas
+        table.innerHTML = `
+    <thead>
+    <tr>
+      <th>ID</th>
+      <th>Fecha</th>
+      <th>Hora</th>
+      <th>Voltaje RMS</th>
+      <th>Línea de Frecuencia</th>
+      <th>Factor de Potencia</th>
+      <th>Corriente RMS</th>
+      <th>Potencia Activa</th>
+      <th>Potencia Reactiva</th>
+      <th>Potencia Aparente</th>
+    </tr>
+    </thead>
+  `;
+
+        // Realiza la solicitud a la API
+        fetch(`/api/data/get?start_date=${startDate}&end_date=${endDate}`)
+            .then(response => response.json())
+            .then(data => {
+                // Crea y agrega las filas a la tabla
+                let tbody = document.createElement("tbody");
+                for (let item of data) {
+                    let row = document.createElement("tr");
+                    row.innerHTML = `
+          <td>${item["ID"]}</td>
+          <td>${item["Fecha"]}</td>
+          <td>${item["Hora"]}</td>
+          <td>${item["Voltaje RMS"]}</td>
+          <td>${item["Línea de Frecuencia"]}</td>
+          <td>${item["Factor de Potencia"]}</td>
+          <td>${item["Corriente RMS"]}</td>
+          <td>${item["Potencia Activa"]}</td>
+          <td>${item["Potencia Reactiva"]}</td>
+          <td>${item["Potencia Aparente"]}</td>
+        `;
+                    tbody.appendChild(row);
+                }
+                table.appendChild(tbody);
+            })
+            .catch(error => console.error('Error:', error));
+    }*/
+
     //const [showGraph, setShowGraph] = useState(false); // Nuevo estado para controlar la visualización de la gráfica
 
     const getconfig = () =>
@@ -474,14 +705,12 @@ const App = function (props) {
         ${h(GraficaLineaFrecuencia, { data })}
         ${h(GraficaPotenciaActivaAparenteReactiva, { data })}
         ${h(GraficaFactorPotencia, { data })}
+        ${h(GraficaFactorPotenciaUltimo, { data })}
         ${h(CurrentChart, { data })}
         ${h(VoltageChart, { data })}
-        `; // Añade esta línea para mostrar u ocultar la gráfica según el estado showGraph
-    /*
-    ${h(Configuration, { config })}
-       ${h(DataList, { data })}
-        */
+        `;
 };
+render(h(App), document.body);
 
 window.onload = () => {
     render(h(App), document.body);
