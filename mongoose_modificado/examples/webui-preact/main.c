8190d059123e1f7ea7a027eff1b7f3e6e0e26f56
Emanuel Rodríguez <75333105+Emanuel-TC@users.noreply.github.com>
2023-06-07 10:03:16 -0600

    correcion de consulta por fechas

--- mongoose_modificado/examples/webui-preact/main.c
+++ mongoose_modificado/examples/webui-preact/main.c
@@ -128,7 +128,7 @@ static char *query_database_20_ultimos(sqlite3 *db) {
 static char *query_database_date(sqlite3 *db, const char *start_date, const char *end_date) {
   sqlite3_stmt *stmt;
   char sql[256];
-  snprintf(sql, sizeof(sql), "SELECT * FROM muestras WHERE Fecha BETWEEN '%s' AND '%s';", start_date, end_date);
+  snprintf(sql, sizeof(sql), "SELECT * FROM muestras WHERE date BETWEEN '%s' AND '%s';", start_date, end_date);
   char *json_result = NULL;
   int rc;
 
@@ -140,7 +140,6 @@ static char *query_database_date(sqlite3 *db, const char *start_date, const char
 
   json_result = strdup("[");
     while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-        int id = sqlite3_column_int(stmt, 0);
         const unsigned char *date = sqlite3_column_text(stmt, 1);
         const unsigned char *time = sqlite3_column_text(stmt, 2);
         double voltageRMS = sqlite3_column_double(stmt, 3);
@@ -152,7 +151,9 @@ static char *query_database_date(sqlite3 *db, const char *start_date, const char
         double apparentp = sqlite3_column_double(stmt, 9);
 
         char buf[512];
-        snprintf(buf, sizeof(buf), "{\"ID\": %d, \"Fecha\": \"%s\", \"Hora\": \"%s\", \"Voltaje RMS\": %f, \"Línea de Frecuencia\": %f, \"Factor de Potencia\": %f, \"Corriente RMS\": %f, \"Potencia Activa\": %f, \"Potencia Reactiva\": %f, \"Potencia Aparente\": %f},", id, date, time, voltageRMS, linef, powerf, currentRMS, activep, reactivep, apparentp);
+        snprintf(buf, sizeof(buf), "{\"date\": \"%s\", \"Hora\": \"%s\", \"Voltaje RMS\": %f, \"Línea de Frecuencia\": %f, \"Factor de Potencia\": %f, \"Corriente RMS\": %f, \"Potencia Activa\": %f, \"Potencia Reactiva\": %f, \"Potencia Aparente\": %f},", date, time, voltageRMS, linef, powerf, currentRMS, activep, reactivep, apparentp);
+
+        /*snprintf(buf, sizeof(buf), "{\"ID\": %d, \"Fecha\": \"%s\", \"Hora\": \"%s\", \"Voltaje RMS\": %f, \"Línea de Frecuencia\": %f, \"Factor de Potencia\": %f, \"Corriente RMS\": %f, \"Potencia Activa\": %f, \"Potencia Reactiva\": %f, \"Potencia Aparente\": %f},", id, date, time, voltageRMS, linef, powerf, currentRMS, activep, reactivep, apparentp);*/
         json_result = realloc(json_result, strlen(json_result) + strlen(buf) + 1);
         strcat(json_result, buf);
   }
@@ -190,17 +191,33 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
       update_config(json, "$.pub", &s_config.pub);
       update_config(json, "$.sub", &s_config.sub);
       mg_http_reply(c, 200, "", "ok\n");
-    } 
-    /*else if (mg_http_match_uri(hm, "/api/data/get")) { // Agregar esta nueva ruta
-      char *json_result = query_database(db); // Llama a la función para consultar la base de datos
-      if (json_result != NULL) {
+    }
+    //
+    else if (mg_http_match_uri(hm, "/api/data/get")) {
+    char start_date[256] = {0}, end_date[256] = {0};
+    mg_http_get_var(&hm->query, "start", start_date, sizeof(start_date));
+    mg_http_get_var(&hm->query, "end", end_date, sizeof(end_date));
+
+    char *json_result = NULL;
+    if (strlen(start_date) > 0 && strlen(end_date) > 0) { 
+        json_result = query_database_date(db, start_date, end_date); // Llama a la función para consultar la base de datos
+        // Debugging print statements
+    	fprintf(stderr, "start_date: %s\n", start_date);
+    	fprintf(stderr, "end_date: %s\n", end_date);
+    } else {
+        json_result = query_database_20_ultimos(db); // Llama a la función para consultar la base de datos
+    }
+
+    if (json_result != NULL) {
         mg_http_reply(c, 200, "Content-Type: application/json\r\n", "%s", json_result);
         free(json_result);
-      } else {
+    } else {
         mg_http_reply(c, 500, "", "Error al consultar la base de datos\n");
-      }
-    }*/
-    else if (mg_http_match_uri(hm, "/api/data/get")) {
+    }
+}
+    //
+     
+    /*else if (mg_http_match_uri(hm, "/api/data/get")) {
     char start_date[256] = {0}, end_date[256] = {0};
     mg_http_get_var(&hm->query, "start", start_date, sizeof(start_date));
     mg_http_get_var(&hm->query, "end", end_date, sizeof(end_date));
@@ -218,7 +235,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
     } else {
         mg_http_reply(c, 500, "", "Error al consultar la base de datos\n");
     }
-   }
+   }*/
 
     else {
       struct mg_http_serve_opts opts = {.root_dir = s_root_dir};
@@ -237,7 +254,7 @@ int main(void) {
 	//sqlite3 *db;
 	int rc;
 
-	rc = sqlite3_open("/home/pi/Documents/servidorTCP/sensor.db", &db);
+	rc = sqlite3_open("/home/emanuel/TT/DataBase/sensor.db", &db);
 
 	if (rc) {
     		fprintf(stderr, "No se puede abrir la base de datos: %s\n", sqlite3_errmsg(db));
